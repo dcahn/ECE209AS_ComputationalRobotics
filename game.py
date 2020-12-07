a6f4b5307de5deb2f4d8a6d97e30a380519967db
twsq <siliconvalley8@gmail.com>
2020-12-06 16:47:01 -0800

    Fix bugs and improve spawning locations for pursuers/evader
    Fixed bugs with not ending the game properly and with spawning
    at edge of board (where there's a wall). Also, improved spawning
    locations for pursuers / evaders by not allowing them to spawn at
    the same location, by enforcing that pursuers can actually reach
    the evader, and that evaders spawn in locations with at least
    two possible moves (to avoid the evader doing nothing).

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from map import Map
-from evader import Evader
+from evader import Evader, board_distance
 from pursuer import Pursuer
 from pursuer_shortest_path import ShortestPathPursuer
 import os
@@ -47,10 +47,38 @@ class Game():
 		self.board = map.makeBoard()
 		self.board_str = ""
 
-		self.evader = Evader(random.randint(1, nrows-1), random.randint(1, ncols-1), self.board)
-        
-		self.pursers = [ShortestPathPursuer(random.randint(1, nrows-1), random.randint(1, ncols-1), self.board) for i in range(npursuers)]
-		# self.pursers = [Pursuer(random.randint(1, nrows-1), random.randint(1, ncols-1), self.board) for i in range(npursuers)]
+		evader_pursuer_locs_valid = False
+		while not evader_pursuer_locs_valid:
+			self.evader = Evader(random.randint(1, nrows-2), random.randint(1, ncols-2), self.board)
+			
+			self.pursers = [ShortestPathPursuer(random.randint(1, nrows-2), random.randint(1, ncols-2), self.board) for i in range(npursuers)]
+			# self.pursers = [Pursuer(random.randint(1, nrows-1), random.randint(1, ncols-1), self.board) for i in range(npursuers)]
+			evader_pursuer_locs_valid = True
+			# Make sure evader does not spawn in a location with three walls / pursuers around it as in this case, the evader 
+			# often does nothing as its only move may be for it to move closer to the pursuers
+			num_evader_moves = 0
+			for a in range(1, 5):
+				evader_cannot_move = False
+				x, y = self.evader.try_action(a)
+				for i in range(npursuers):
+					if self.evader.getPos() == (x, y) or self.pursers[i].getPos() == (x, y):
+						evader_cannot_move = True
+				if not evader_cannot_move:
+					num_evader_moves += 1
+			if num_evader_moves <= 1:
+				evader_pursuer_locs_valid = False
+				continue
+			# Make sure evaders and pursuers all spawn at different locations and that pursuers can actually 
+			# reach evader
+			evader_pursuer_positions = set()
+			evader_pursuer_positions.add(self.evader.getPos())
+			for i in range(npursuers):
+				if self.pursers[i].getPos() in evader_pursuer_positions:
+					evader_pursuer_locs_valid = False
+				if board_distance(self.evader.getPos(), self.pursers[i].getPos(), self.board) is None:
+					evader_pursuer_locs_valid = False
+				evader_pursuer_positions.add(self.pursers[i].getPos())
+				
 		self.updateBoard()
 
 	def board_to_string(self):
@@ -84,12 +112,11 @@ class Game():
 	
 	# Update board using pursuer and evader position
 	def updateBoard(self):
+		x, y = self.evader.getPos()
+		self.board[x][y] = 'e'
 		for purser in self.pursers:
 			x, y = purser.getPos()
 			self.board[x][y] = 'p'
-		
-		x, y = self.evader.getPos()
-		self.board[x][y] = 'e'
 
 		self.board_to_string()
 	
@@ -107,14 +134,14 @@ class Game():
 		print(self.board_str)
 
 	def done(self):
-		#for purser in self.pursers:
-		#	if self.evader.getPos() == purser.getPos():
-		#		return True
-		for i in range(len(self.board)):
-			for j in range(len(self.board[0])):
-				if self.board[i][j] == '.':
-					return False
-		return True
+		for purser in self.pursers:
+			if self.evader.getPos() == purser.getPos():
+				return True
+		# for i in range(len(self.board)):
+		# 	for j in range(len(self.board[0])):
+		# 		if self.board[i][j] == '.':
+		# 			return False
+		return False
 
 
 if __name__ == "__main__":
