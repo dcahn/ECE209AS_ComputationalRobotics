0f994267c95886590c8969c690993e03e791812d
ChengTseLu <72141509+ChengTseLu@users.noreply.github.com>
2020-12-08 10:15:20 +0800

    remove policy function

--- pursuer.py
+++ pursuer.py
@@ -10,13 +10,13 @@ class Pursuer:
     # decode action
     def move(self, a):
         if a == 1:
-            return -1, 0    # left
+            return -1, 0    # up
         elif a == 2:
-            return 1, 0     # right
+            return 1, 0     # down
         elif a == 3:
-            return 0, -1    # down
+            return 0, -1    # left
         elif a == 4:
-            return 0, 1     # up
+            return 0, 1     # right
 
         return 0, 0         # invalid action
 
@@ -35,76 +35,7 @@ class Pursuer:
     def print(self):
         print(self.x, self.y)
     
-    # policy (random action for now)
-    def Policy(self, b):
-        self.board = copy.deepcopy(b)
-        self.init_MDP()
-        
-        # run value iteration for 50 times
-        for i in range(50):
-            self.value = self.VI()
-
-        return self.policy[self.x][self.y]
-
-
-    # initial value, reward, and policy for all states
-    def init_MDP(self):
-        self.value = copy.deepcopy(self.board)
-        self.reward = copy.deepcopy(self.board)
-        self.policy = copy.deepcopy(self.board)
-
-        # set reward and value
-        for x in range(len(self.value)):
-            for y in range(len(self.value[x])):
-                if self.value[x][y] == '.' or self.value[x][y] == ' ' or self.value[x][y] == 'p':
-                    self.value[x][y] = 0
-                    self.reward[x][y] = 0
-
-                elif self.value[x][y] == 'e':
-                    self.value[x][y] = 100
-                    self.reward[x][y] = 0
-    
-    # value iteration
-    def VI(self):
-        value_temp = copy.deepcopy(self.value)
-
-        # loop entire array
-        for x in range(len(self.value)):
-            for y in range(len(self.value[x])):
-
-                # skip if the cell are walls or or final position
-                if isinstance(self.value[x][y], str) or self.board[x][y] == 'e':
-                    continue
-                else:
-                    # loop 4 actions and calculate each Q(s,a)
-                    v_lst = []
-                    for i in range(4):
-                        dx, dy = self.move(i+1)
-                        v = self.value[x+dx][y+dy]
-                        r = self.reward[x+dx][y+dy]
-
-                        # if the action will hit the wall, stay in current position
-                        if isinstance(v, str) or isinstance(r, str):
-                            v = self.value[x][y]
-                            r = self.reward[x][y]
-
-                        # Q(s,a)
-                        v_lst.append(self.Bellman_Backup(r, v, 0.8))
-                    
-                    # choose Q_max(s,a) for value and policy
-                    value_temp[x][y] = max(v_lst)
-                    self.policy[x][y] = v_lst.index(max(v_lst)) + 1
-
-        return value_temp
-
-    # Bellman Backup
-    def Bellman_Backup(self, r, v, df):
-        return r + df*v
-
-
-        
-
-
+   
 
 if __name__ == "__main__":
     # test
